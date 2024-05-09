929b5c44e5776307824736d856e721553b2b2ef9
Abin <112235416+iam-abin@users.noreply.github.com>
2024-05-09 08:46:22 +0530

    Update categoryFetching.js

--- middlewares/categoryFetching.js
+++ middlewares/categoryFetching.js
@@ -4,7 +4,7 @@ const { getAllcategory } = require('../helpers/categoryHelper');
 const categorySupply = async (req, res, next) => {
     try {
         const allCategories = await getAllcategory();
-        res.locals.allCategories = allCategories;
+        res.locals.allCategories = allCategories; // Pass 'allCategories' data directly to views. Its scope is current request.
         next();
     } catch (error) {
         next(error)
